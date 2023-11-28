2c523e478aa0b4af06108215fa1da1e68c525c51
GaylordCC <gcarricaballero3@hotmail.com>
2023-11-28 15:46:04 -0500

    Implemenhtation of fuctions to: unplublished, return blog's id, fetch comments from a blog and create a blog

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'Sarthak'}}
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # Only get 10 published blogs
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
 
 
-@app.get('/about')
-def about():
-    return {'data': {'about page'}}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blog'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # Fetch blog with id = id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    # Fetch comments from a blog with id = id
+    return {'data':  {'1', '2'}}
+
+class Blog(BaseModel):
+    tittle: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data': 'Blog is created'}
