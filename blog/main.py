2de1db4a36a1df0d060bbea1aa7c0d2a27508cf5
GaylordCC <gcarricaballero3@hotmail.com>
2023-11-30 22:11:52 -0500

    Correction of blog data storage procedure

--- blog/main.py
+++ blog/main.py
@@ -8,17 +8,16 @@ app = FastAPI()
 models.Base.metadata.create_all(engine)
 
 def get_db():
-    db = SessionLocal
+    db = SessionLocal()  # Create an instance of SessionLocal
     
     try:
         yield db
     finally:
         db.close()
 
-
 @app.post('/blog')
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title,body=request.body)
+    new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
