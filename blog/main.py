9607e4f73d98173f209b8341d8b239cfc0703e02
GaylordCC <gcarricaballero3@hotmail.com>
2023-12-03 14:40:48 -0500

    Include some comment on the code

--- blog/main.py
+++ blog/main.py
@@ -25,6 +25,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
+#Delete a blog
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id,db: Session = Depends(get_db)):
     db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
@@ -32,6 +33,7 @@ def destroy(id,db: Session = Depends(get_db)):
     return 'done'
 
 
+# Update blog
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
@@ -39,7 +41,6 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
 
-    # blog.update(request)
     blog.title = request.title
     blog.body = request.body
     db.commit()
