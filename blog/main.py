4030a1960233085b746a37d3283453925d2dc6a7
GaylordCC <gcarricaballero3@hotmail.com>
2023-12-03 14:24:13 -0500

    Implementation of update endpoint

--- blog/main.py
+++ blog/main.py
@@ -25,6 +25,27 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id,db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+
+    # blog.update(request)
+    blog.title = request.title
+    blog.body = request.body
+    db.commit()
+    return 'updated'
+
+
 # Search a blog
 @app.get('/blog')
 def all(db: Session = Depends(get_db)):
