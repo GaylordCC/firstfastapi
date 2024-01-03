2c0f34a0ec7ba1799136418f7c0d0b89e8fb3200
GaylordCC <gcarricaballero3@hotmail.com>
2024-01-03 16:31:50 -0500

    Implementation of response model method to the app

--- blog/main.py
+++ blog/main.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
@@ -5,7 +6,9 @@ from sqlalchemy.orm import Session
 
 app = FastAPI()
 
+# Create the database tables
 models.Base.metadata.create_all(engine)
+
 
 def get_db():
     db = SessionLocal()  # Create an instance of SessionLocal
@@ -15,7 +18,8 @@ def get_db():
     finally:
         db.close()
 
-# Database storage of a created blog
+
+# Created a blog and storage in the database
 @app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
@@ -32,7 +36,6 @@ def destroy(id,db: Session = Depends(get_db)):
 
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
-
 
     # blog.delete(synchronize_session=False)
     db.delete(blog)
@@ -56,14 +59,14 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
 
 
 # Search a blog
-@app.get('/blog')
+@app.get('/blog', response_model = List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
 # Find a specyfic blog by id
-@app.get('/blog/{id}', status_code=200)
+@app.get('/blog/{id}', status_code=200, response_model = schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
