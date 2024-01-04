7e1269d42e27b87306b72b85d413951ef3b7d586
GaylordCC <gcarricaballero3@hotmail.com>
2024-01-03 19:44:07 -0500

    Use of tags in the endpoints

--- blog/main.py
+++ blog/main.py
@@ -22,7 +22,7 @@ def get_db():
 
 
 # Created a blog and storage in the database
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
+@app.post('/blog', status_code=status.HTTP_201_CREATED, tags=['blogs'])
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -32,7 +32,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 
 #Delete a blog
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['blogs'])
 def destroy(id,db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
 
@@ -46,7 +46,7 @@ def destroy(id,db: Session = Depends(get_db)):
 
 
 # Update blog
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED, tags=['blogs'])
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
 
@@ -61,14 +61,14 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
 
 
 # Search a blog
-@app.get('/blog', response_model = List[schemas.ShowBlog])
+@app.get('/blog', response_model = List[schemas.ShowBlog], tags=['blogs'])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
 # Find a specyfic blog by id
-@app.get('/blog/{id}', status_code=200, response_model = schemas.ShowBlog)
+@app.get('/blog/{id}', status_code=200, response_model = schemas.ShowBlog, tags=['blogs'])
 def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -81,12 +81,19 @@ def show(id, response: Response, db: Session = Depends(get_db)):
 
 
 
-
-
-@app.post('/user')
+@app.post('/user', response_model = schemas.ShowUser, tags=['users'])
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(name=request.name,email=request.email,password=Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get('/user', response_model = schemas.ShowUser, tags=['users'])
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"User with the id {id} is not available")
+    
+    return user
