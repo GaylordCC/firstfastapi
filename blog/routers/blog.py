e6b3c3ba51e6c8ed92e3af44847f18694c966b2d
GaylordCC <gcarricaballero3@hotmail.com>
2024-01-15 23:35:07 -0500

    Routes behind authentication

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,6 +1,6 @@
 from typing import List
 from fastapi import APIRouter, Depends, status, Response
-from .. import schemas, database
+from .. import schemas, database, oauth2
 from sqlalchemy.orm import Session
 from ..repository import blog
 
@@ -12,23 +12,23 @@ router = APIRouter(
 
 # Search all blogs created by an user
 @router.get('/', response_model = List[schemas.ShowBlog])
-def all(db: Session = Depends(database.get_db)):
+def all(db: Session = Depends(database.get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.get_all(db)
 
 # Created a blog and storage in the database
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(database.get_db)):
+def create(request: schemas.Blog, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     blog.create(request, db)
 
 #Delete a blog
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id: int,db: Session = Depends(database.get_db)):
+def destroy(id: int,db: Session = Depends(database.get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.destroy(id, db)
 
 
 # Update blog
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id: int, request: schemas.Blog, db: Session = Depends(database.get_db)):
+def update(id: int, request: schemas.Blog, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.update(id, request, db)
 
 
@@ -41,5 +41,5 @@ def update(id: int, request: schemas.Blog, db: Session = Depends(database.get_db
 
 # Find a specific blog by id
 @router.get('/{id}', status_code=200, response_model = schemas.ShowBlog)
-def show(id: int, response: Response, db: Session = Depends(database.get_db)):
+def show(id: int, response: Response, db: Session = Depends(database.get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.show(id, db)
